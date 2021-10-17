180eae53a9963224a2792dfc2b94b6b30a76df5f
Fauzi Jabbar <fauzi.jabbarfaisal@luxodev.com>
2021-10-17 20:14:02 +0700

    revisi nomor wa

--- consts/config.js
+++ consts/config.js
@@ -1,3 +1,3 @@
 const isiSMS = "Saya+ingin+berkonsultasi+sebelum+mengikuti+kelas+Belajar+Saham+Syariah%202021"
-const nomorHP = "081349480121"
+const nomorHP = "6281349480121"
 export const isiLinkWA = `https://api.whatsapp.com/send/?phone=${nomorHP}&text=${isiSMS}`
